3d95538648f97f92a8829ccf9f9c5b8cf591e48e
Andre Silva <andre.beat@gmail.com>
2015-10-14 01:57:00 +0100

    implement front and back

--- src/fingers.rs
+++ src/fingers.rs
@@ -1,20 +1,23 @@
-use std::iter::FromIterator;
+use std::iter::{FromIterator, Iterator};
 use std::rc::Rc;
+
+//
+// --- Traits
+//
 
 pub trait Monoid {
     fn zero() -> Self;
     fn append(&self, a: Self) -> Self;
 }
 
-impl Monoid for usize {
-    fn zero() -> usize { 0 }
-    fn append(&self, a: usize) -> usize { self + a }
-}
-
 pub trait Foldable<A> {
     fn foldl<B, F>(&self, z: B, f: F) -> B where F: Fn(B, &A) -> B;
     fn foldr<B, F>(&self, z: B, f: F) -> B where F: Fn(&A, B) -> B;
 }
+
+//
+// --- Data Types
+//
 
 // TODO: Overall think about what king of subtree sharing the current usage of Rc permits
 //       Should all the values of Digit use Rc? Should the Single and Digit's in Deep use Rc?
@@ -48,6 +51,40 @@ use fingers::FingerTree::*;
 use fingers::Node::*;
 use fingers::Digit::*;
 
+//
+// --- Macros
+//
+
+#[macro_export]
+macro_rules! finger_tree {
+    ($( $v: expr ),*) => {{
+         let tree = FingerTree::new();
+         tree $( .push_back($v) )*
+    }}
+}
+
+//
+// --- Implementations
+//
+
+impl<A> Node<A> {
+    fn front(&self) -> &A {
+        match *self {
+            Leaf(ref a) => a,
+            Node2(ref a, _) => a.front(),
+            Node3(ref a, _, _) => a.front(),
+        }
+    }
+
+    fn back(&self) -> &A {
+        match *self {
+            Leaf(ref a) => a,
+            Node2(_, ref a) => a.back(),
+            Node3(_, _, ref a) => a.back(),
+        }
+    }
+}
+
 impl<A> Foldable<A> for Node<A> {
     fn foldl<B, F>(&self, z: B, f: F) -> B where F: Fn(B, &A) -> B {
         match *self {
@@ -62,6 +99,26 @@ impl<A> Foldable<A> for Node<A> {
             Leaf(ref a) => f(a, z),
             Node2(ref a, ref b) => a.foldr(b.foldr(z, &f), &f),
             Node3(ref a, ref b, ref c) => a.foldr(b.foldr(c.foldr(z, &f), &f), &f)
+        }
+    }
+}
+
+impl<A> Digit<A> {
+    fn front(&self) -> &A {
+        match *self {
+            One(ref a) => a.front(),
+            Two(ref a, _) => a.front(),
+            Three(ref a, _, _) => a.front(),
+            Four(ref a, _, _, _) => a.front()
+        }
+    }
+
+    fn back(&self) -> &A {
+        match *self {
+            One(ref a) => a.back(),
+            Two(_, ref a) => a.back(),
+            Three(_, _, ref a) => a.back(),
+            Four(_, _, _, ref a) => a.back()
         }
     }
 }
@@ -82,6 +139,80 @@ impl<A> Foldable<A> for Digit<A> {
             Two(ref a, ref b) => a.foldr(b.foldr(z, &f), &f),
             Three(ref a, ref b, ref c) => a.foldr(b.foldr(c.foldr(z, &f), &f), &f),
             Four(ref a, ref b, ref c, ref d) => a.foldr(b.foldr(c.foldr(d.foldr(z, &f), &f), &f), &f)
+        }
+    }
+}
+
+impl<A: Clone> FingerTree<A> {
+    pub fn new() -> FingerTree<A> {
+        Empty
+    }
+
+    pub fn push_front(&self, a: A) -> FingerTree<A> {
+        self.push_front_aux(Leaf(a))
+    }
+
+    fn push_front_aux(&self, a: Node<A>) -> FingerTree<A> {
+        match *self {
+            Empty => Single(a),
+            Single(ref b) => Deep(One(a), Rc::new(Empty), One(b.clone())),
+            Deep(ref pr, ref m, ref sf) =>
+                match *pr {
+                    One(ref b) =>
+                        Deep(Two(a, b.clone()), m.clone(), sf.clone()),
+                    Two(ref b, ref c) =>
+                        Deep(Three(a, b.clone(), c.clone()), m.clone(), sf.clone()),
+                    Three(ref b, ref c, ref d) =>
+                        Deep(Four(a, b.clone(), c.clone(), d.clone()), m.clone(), sf.clone()),
+                    Four(ref b, ref c, ref d, ref e) =>
+                        Deep(
+                            Two(a, b.clone()),
+                            Rc::new(m.push_front_aux(Node3(Rc::new(c.clone()), Rc::new(d.clone()), Rc::new(e.clone())))),
+                            sf.clone()),
+                }
+
+        }
+    }
+
+    pub fn push_back(&self, a: A) -> FingerTree<A> {
+        self.push_back_aux(Leaf(a))
+    }
+
+    fn push_back_aux(&self, a: Node<A>) -> FingerTree<A> {
+        match *self {
+            Empty => Single(a),
+            Single(ref b) => Deep(One(b.clone()), Rc::new(Empty), One(a)),
+            Deep(ref pr, ref m, ref sf) =>
+                match *sf {
+                    One(ref b) =>
+                        Deep(pr.clone(), m.clone(), Two(b.clone(), a)),
+                    Two(ref c, ref b) =>
+                        Deep(pr.clone(), m.clone(), Three(c.clone(), b.clone(), a)),
+                    Three(ref d, ref c, ref b) =>
+                        Deep(pr.clone(), m.clone(), Four(d.clone(), c.clone(), b.clone(), a)),
+                    Four(ref e, ref d, ref c, ref b) =>
+                        Deep(
+                            pr.clone(),
+                            Rc::new(m.push_back_aux(Node3(Rc::new(e.clone()), Rc::new(d.clone()), Rc::new(c.clone())))),
+                            Two(b.clone(), a))
+                }
+
+        }
+    }
+
+    pub fn front(&self) -> Option<&A> {
+        match *self {
+            Empty => None,
+            Single(ref a) => Some(a.front()),
+            Deep(ref pr, _, _) => Some(pr.front())
+        }
+    }
+
+    pub fn back(&self) -> Option<&A> {
+        match *self {
+            Empty => None,
+            Single(ref a) => Some(a.back()),
+            Deep(_, _, ref sf) => Some(sf.back())
         }
     }
 }
@@ -113,64 +244,6 @@ impl<A> Foldable<A> for FingerTree<A> {
                 fold(m.foldr(fold(z, sf, &f), |a, b| f(a, b)), pr, &f)
             }
         }
-    }
-}
-
-impl<A: Clone> FingerTree<A> {
-    fn new() -> FingerTree<A> {
-        Empty
-    }
-
-    fn push_front_aux(&self, a: Node<A>) -> FingerTree<A> {
-        match *self {
-            Empty => Single(a),
-            Single(ref b) => Deep(One(a), Rc::new(Empty), One(b.clone())),
-            Deep(ref pr, ref m, ref sf) =>
-                match *pr {
-                    One(ref b) =>
-                        Deep(Two(a, b.clone()), m.clone(), sf.clone()),
-                    Two(ref b, ref c) =>
-                        Deep(Three(a, b.clone(), c.clone()), m.clone(), sf.clone()),
-                    Three(ref b, ref c, ref d) =>
-                        Deep(Four(a, b.clone(), c.clone(), d.clone()), m.clone(), sf.clone()),
-                    Four(ref b, ref c, ref d, ref e) =>
-                        Deep(
-                            Two(a, b.clone()),
-                            Rc::new(m.push_front_aux(Node3(Rc::new(c.clone()), Rc::new(d.clone()), Rc::new(e.clone())))),
-                            sf.clone()),
-                }
-
-        }
-    }
-
-    pub fn push_front(&self, a: A) -> FingerTree<A> {
-        self.push_front_aux(Leaf(a))
-    }
-
-    fn push_back_aux(&self, a: Node<A>) -> FingerTree<A> {
-        match *self {
-            Empty => Single(a),
-            Single(ref b) => Deep(One(b.clone()), Rc::new(Empty), One(a)),
-            Deep(ref pr, ref m, ref sf) =>
-                match *sf {
-                    One(ref b) =>
-                        Deep(pr.clone(), m.clone(), Two(b.clone(), a)),
-                    Two(ref c, ref b) =>
-                        Deep(pr.clone(), m.clone(), Three(c.clone(), b.clone(), a)),
-                    Three(ref d, ref c, ref b) =>
-                        Deep(pr.clone(), m.clone(), Four(d.clone(), c.clone(), b.clone(), a)),
-                    Four(ref e, ref d, ref c, ref b) =>
-                        Deep(
-                            pr.clone(),
-                            Rc::new(m.push_back_aux(Node3(Rc::new(e.clone()), Rc::new(d.clone()), Rc::new(c.clone())))),
-                            Two(b.clone(), a))
-                }
-
-        }
-    }
-
-    pub fn push_back(&self, a: A) -> FingerTree<A> {
-        self.push_back_aux(Leaf(a))
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate fingers;
 
 use fingers::fingers::FingerTree; // TODO: this is dumb :-(
@@ -7,4 +8,10 @@ fn main() {
     let n_vec = vec![0, 1, 2, 3, 4];
     let n_tree = FingerTree::<usize>::from_iter(n_vec);
     println!("{:?}", n_tree);
+
+    let n_tree2 = finger_tree![0, 1, 2, 3, 4];
+    println!("{:?}", n_tree2);
+
+    println!("front: {:?}", n_tree.front());
+    println!("back: {:?}", n_tree.back());
 }
